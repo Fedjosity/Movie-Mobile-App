efd2fc39a6b6e0d2a5027099814ea21c96e17887
Fedjosity <fedjostayomide@gmail.com>
2025-12-11 10:57:53 +0100

    Update app icon references to use a new logo image, enhance tab layout with improved styling options, and modify the main index screen to include a background image and logo display.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-primary font-bold">Welcome</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"></Image>
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
