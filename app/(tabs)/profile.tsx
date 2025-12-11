3b437e6b51927587b72e5a59a652c8063f4f71db
Fedjosity <fedjostayomide@gmail.com>
2025-12-11 10:33:00 +0100

    Enhance Tailwind configuration with custom color palette, update layout to include additional screens, and remove unused index file.

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Moviedetails = () => {
+  return (
+    <View>
+      <Text>Moviedetails</Text>
+    </View>
+  );
+};
+
+export default Moviedetails;
+
+const styles = StyleSheet.create({});
