4b13d888a80768f1a252da45b1f79c71fc594a7c
Fedjosity <fedjostayomide@gmail.com>
2025-12-12 13:24:19 +0100

    Implement movie fetching functionality with useFetch hook, update API configuration to use access token, and enhance index screen to display loading state and error handling for movie data.

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 export const TMDB_CONFIG = {
   BASE_URL: "https://api.themoviedb.org/3",
-  API_KEY: process.env.EXPO_PUBLIC_TMDB_API_KEY,
-  Headers: {
+  ACCESS_TOKEN: process.env.EXPO_PUBLIC_TMDB_ACCESS_TOKEN,
+  headers: {
     accept: "application/json",
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_ACCESS_TOKEN}`,
   },
@@ -9,12 +9,12 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `%{TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `%{TMDB_CONFIG.BASE_URL}/discover/movie/?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
   const response = await fetch(endpoint, {
     method: "GET",
-    headers: TMDB_CONFIG.Headers,
+    headers: TMDB_CONFIG.headers,
   });
 
   if (!response.ok) {
