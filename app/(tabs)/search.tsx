269d5ec0e4bc64f071e98a0ff63cb340b882e274
Fedjosity <fedjostayomide@gmail.com>
2025-12-13 17:41:54 +0100

    Update search tab to include background image and improve layout styling

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,21 @@
+import { images } from "@/constants/images";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, TextInput, View } from "react-native";
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList>
+        <TextInput
+          placeholder="Search for movies"
+          className="bg-white rounded-lg p-3 mx-5 mt-5"
+        />
+      </FlatList>
     </View>
   );
 };
